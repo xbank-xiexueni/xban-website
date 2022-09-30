18805d3af71418a495e500f2c02dcf46b3a875c1
xiexueni <xiexueni@xbank.plus>
2022-09-30 18:29:51 +0800

    feat: add keyword

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,7 +8,8 @@ const config: GatsbyConfig = {
     description:
       'xBank is the first Web3 and Crypto platform that takes you into the best Web3 projects & cryptos including NFT, Game, DAO, digital collectibles and Web3 community. ',
     twitterUsername: `xBank_Official`,
-    keyword: '',
+    keyword:
+      'best web 3 projects,web 3 benefits,web3 community,xbank3.0,xbank,xbank web3,xbank plus',
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
